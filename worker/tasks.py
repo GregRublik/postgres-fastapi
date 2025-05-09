82f139bf8b72504f36d34399c49c4d84a5f0d142
Grigory Ivanov <grigorashekivanov2@gmail.com>
2025-05-09 19:40:58 +0300

    добавил повторную обработку задачи чтобы чтобы в случае ошибки было еще 2 попытки

--- worker/tasks.py
+++ worker/tasks.py
@@ -1,7 +1,13 @@
 from selery_app import app
 
 
-@app.task(name='string', queue='first_message')
-def process_message(message):
+@app.task(
+    name='string',
+    queue='first_message',
+    bind=True,
+    max_retries=3
+)
+def process_message(self, message):
+    print(self.request)
     print(f"Received message: {message}")
     return {"status": "success", "message": message}
